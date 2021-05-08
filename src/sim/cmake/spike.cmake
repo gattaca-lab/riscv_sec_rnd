92e5f7da91f3daae1f3d372c65f3ac82344653fa
Alex White <space.monkey.delivers@gmail.com>
2021-05-08 20:27:08 +0300

    Added option to enable/disable building simulators

--- src/sim/cmake/spike.cmake
+++ src/sim/cmake/spike.cmake
@@ -6,14 +6,16 @@ ProcessorCount(N)
 set(SPIKE_SRC_DIR "${SIM_SRC_DIR}/spike/")
 set(SPIKE_INSTALL_DIR "${INSTALL_DIR}/spike/")
 
-# Create build and install directories
-file(MAKE_DIRECTORY ${SPIKE_INSTALL_DIR})
-
-ExternalProject_Add(
-    spike
-    PREFIX ${SPIKE_INSTALL_DIR}
-    SOURCE_DIR ${SPIKE_SRC_DIR}
-    CONFIGURE_COMMAND ${SPIKE_SRC_DIR}/configure --prefix=${SPIKE_INSTALL_DIR}
-    BUILD_COMMAND make -j${N}
-    INSTALL_COMMAND make install -j${N}
-)
+# Function to build spike simulator
+function(build_sim_spike EXTRA_CONFIG_ARGS)
+  # Create build and install directories
+  file(MAKE_DIRECTORY ${SPIKE_INSTALL_DIR})
+  ExternalProject_Add(
+      spike
+      PREFIX ${SPIKE_INSTALL_DIR}
+      SOURCE_DIR ${SPIKE_SRC_DIR}
+      CONFIGURE_COMMAND ${SPIKE_SRC_DIR}/configure --prefix=${SPIKE_INSTALL_DIR} ${EXTRA_CONFIG_ARGS}
+      BUILD_COMMAND make -j${N}
+      INSTALL_COMMAND make install -j${N}
+  )
+endfunction()
